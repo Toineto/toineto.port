39f4409d4f52f2001ee5894d57ba6c216688b101
toineto <augusto.vilhena4@gmail.com>
2022-07-26 22:20:01 -0300

    add refatoração

---
+++ toin/src/components/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Skills() {
+    return (
+        <>
+            <section className="section">
+                <h1>Skills</h1>
+            </section>
+        </>
+    )
+}
+
+export default Skills;
---
+++ toin/src/pages/MainPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import About from '../components/About';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Home from '../components/Home';
+import Projects from '../components/Projects';
+import Skills from '../components/Skills';
+
+const MainPage = () => (
+  <div>
+    <Header />
+    <Home />
+    <About />
+    <Projects />
+    <Skills />
+    <Footer />
+  </div>
+)
+
+
+export default MainPage;
